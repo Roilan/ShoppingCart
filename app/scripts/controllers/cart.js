71822fecc21b7cb5514222425205fa970728529d
Roilan <roilan14@gmail.com>
2015-03-11 11:04:56 -0400

    added static data, template and styles for cart

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -9,7 +9,25 @@
  */
 angular.module('shoppingCartApp')
   .controller('ShoppingCartController', function ($scope) {
+    // cart status - currently static
+    $scope.status = 'Open';
 
-    $scope.name = 'working';
-
+    // cart items
+    $scope.items = [
+      {
+        name: 'item1',
+        amount: 10,
+        price: 9.99
+      },
+      {
+        name: 'item2',
+        amount: 5,
+        price: 10.99
+      },
+      {
+        name: 'item3',
+        amount: 100,
+        price: 11.01
+      }
+    ];
   });

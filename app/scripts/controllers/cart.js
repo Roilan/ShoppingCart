15a71d513bd8a983aaca7fe86473ad34aafa4426
Roilan <roilan14@gmail.com>
2015-03-11 18:34:42 -0400

    added total price and calculates total price when added to shopping cart

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -22,17 +22,20 @@ angular.module('shoppingCartApp')
       {
         name: 'Food',
         amount: 0,
-        price: 9.99
+        price: 9.99,
+        totalprice: 0
       },
       {
         name: 'Accessories',
         amount: 0,
-        price: 10.99
+        price: 10.99,
+        totalprice: 0
       },
       {
         name: 'Care',
         amount: 0,
-        price: 11.01
+        price: 11.01,
+        totalprice: 0
       }
     ];
 
@@ -44,13 +47,22 @@ angular.module('shoppingCartApp')
       $scope.cartItems = [];
     };
 
-    // pushes item into the cart
-    $scope.addToCart = function(item) {
+    // calculates item total
+    $scope.calculateItemTotal = function(item) {
+      var totalPrice = item.amount * item.price;
+      return parseFloat(Math.round(totalPrice * 100) / 100).toFixed(2);
+    };
+
+    // pushes item into the cartItems array
+    $scope.addToCart = function(item, $index) {
       $scope.cartItems.push(item);
+      $scope.cartItems[$index].totalprice = $scope.calculateItemTotal(item);
     };
 
     // removes item from cartItems array
     $scope.removeCartItem = function($index) {
       $scope.cartItems.splice($index, 1);
     };
+
+
   });
